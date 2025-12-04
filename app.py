5026af1c5b9887c2bbf4649d986a9107bf632957
Abhay420000 <abhaykumar420000@gmail.com>
2025-12-04 09:18:19 +0530

    Initial commit - Website V1

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# --- 1. HOMEPAGE ---
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# --- 2. ARTICLE ROUTES ---
+# EVERY TIME you make a new video, you will add a new block here.
+# The string inside @app.route('...') is what appears in the browser URL.
+
+@app.route('/how-to-deploy-flask')
+def article_deploy_flask():
+    # This points to the file inside templates/articles/ folder
+    return render_template('articles/deploy_flask.html')
+
+# Add this to app.py
+@app.route('/service-console-tools')
+def article_service_console():
+    return render_template('articles/service_console_tools.html')
+
+# Example for your next video (Uncomment when needed):
+# @app.route('/python-variables-tutorial')
+# def article_python_vars():
+#     return render_template('articles/python_vars.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
