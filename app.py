1a8e1c8d9ec49e2d69f941f30c4c4b9b9665962d
Abhay420000 <abhaykumar420000@gmail.com>
2025-12-04 18:38:02 +0530

    Updated!

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def home():
 # The string inside @app.route('...') is what appears in the browser URL.
 
 # Add this to app.py
-@app.route('/service-console-tools')
+@app.route('/quick-text-in-salesforce')
 def article_service_console():
     return render_template('articles/service_console_tools_quickText.html')
 
-# Example for your next video (Uncomment when needed):
-# @app.route('/python-variables-tutorial')
-# def article_python_vars():
-#     return render_template('articles/python_vars.html')
+# Article 2: Apex Transactions
+@app.route('/how-to-manage-transactions-in-salesforce-apex')
+def article_apex_transactions():
+    return render_template('articles/apex_transactions.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
